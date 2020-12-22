0d39841884647e506e67f212c2d525d65ade61f6
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-12-22 19:59:39 +0200

    create elements for svg graphics

---
+++ QuantumPuzzleMechanics/Graphics.Elems.fs
@@ -0,0 +1,286 @@
+﻿module QuantumPuzzleMechanics.Graphics.Elems
+
+open System
+
+open Giraffe.ViewEngine
+open Xamarin.Forms
+
+let lineWidth (size: float): string =
+    size
+    |> (*) 0.05
+    |> string
+
+let rgbVal (v: float): string =
+    v
+    |> (*) 100.0
+    |> Math.Round
+    |> int
+    |> string
+
+let rgb(color: Color): string =
+    sprintf "rgb(%s, %s, %s)"
+            (rgbVal color.R)
+            (rgbVal color.G)
+            (rgbVal color.B)
+
+let transform (scale: float) (size: float): string =
+        sprintf "translate(%s,%s)"
+                (size |> (*) scale |> string)
+                (size |> (*) scale |> string)
+
+let horizWire (color: Color) (size: float): XmlNode =
+    let stroke = rgb color
+    let x1 = string 0
+    let y1 = size |> (*) 0.5 |> string
+    let x2 = string size
+    let y2 = size |> (*) 0.5 |> string
+    let strokeWidth = lineWidth size
+    tag "line"
+        [ attr "stroke" stroke
+          attr "x1" x1
+          attr "y1" y1
+          attr "x2" x2
+          attr "y2" y2
+          attr "stroke-width" strokeWidth ]
+        []
+
+let vertWire (color: Color) (size: float): XmlNode =
+    let stroke = rgb color
+    let x1 = size |> (*) 0.5 |> string
+    let y1 = string 0
+    let x2 = size |> (*) 0.5 |> string
+    let y2 = string size
+    let strokeWidth = lineWidth size
+    tag "line"
+        [ attr "stroke" stroke
+          attr "x1" x1
+          attr "y1" y1
+          attr "x2" x2
+          attr "y2" y2
+          attr "stroke-width" strokeWidth ]
+        []
+
+let topVertWire (color: Color) (size: float): XmlNode =
+    let stroke = rgb color
+    let x1 = size |> (*) 0.5 |> string
+    let y1 = string 0
+    let x2 = size |> (*) 0.5 |> string
+    let y2 = size |> (*) 0.5 |> string
+    let strokeWidth = lineWidth size
+    tag "line"
+        [ attr "stroke" stroke
+          attr "x1" x1
+          attr "y1" y1
+          attr "x2" x2
+          attr "y2" y2
+          attr "stroke-width" strokeWidth ]
+        []
+
+let bottomVertWire (color: Color) (size: float): XmlNode =
+    let stroke = rgb color
+    let x1 = size |> (*) 0.5 |> string
+    let y1 = size |> (*) 0.5 |> string
+    let x2 = size |> (*) 0.5 |> string
+    let y2 = string size
+    let strokeWidth = lineWidth size
+    tag "line"
+        [ attr "stroke" stroke
+          attr "x1" x1
+          attr "y1" y1
+          attr "x2" x2
+          attr "y2" y2
+          attr "stroke-width" strokeWidth ]
+        []
+
+let controlSymbol (color: Color) (size: float): XmlNode =
+    let fill = rgb color
+    let cx = size |> (*) 0.5 |> string
+    let cy = size |> (*) 0.5 |> string
+    let r = size |> (*) 0.15 |> string
+    tag "circle"
+        [ attr "fill" fill
+          attr "cx" cx
+          attr "cy" cy
+          attr "r" r ]
+        []
+
+let notSymbol (color: Color) (size: float): XmlNode =
+    let innerSize = 0.2 * size
+    let stroke = rgb color
+    let strokeWidth = lineWidth size
+    let innerHorizWire =
+            let x1 = innerSize |> (*) -1.0 |> string
+            let y1 = string 0
+            let x2 = string innerSize
+            let y2 = string 0
+            tag "line"
+                [ attr "stroke" stroke
+                  attr "x1" x1
+                  attr "y1" y1
+                  attr "x2" x2
+                  attr "y2" y2
+                  attr "stroke-width" strokeWidth ]
+                []
+    let innerVertWire =
+            let x1 = string 0
+            let y1 = innerSize |> (*) -1.0 |> string
+            let x2 = string 0
+            let y2 = string innerSize
+            tag "line"
+                [ attr "stroke" stroke
+                  attr "x1" x1
+                  attr "y1" y1
+                  attr "x2" x2
+                  attr "y2" y2
+                  attr "stroke-width" strokeWidth ]
+                []
+    let innerCircle =
+            let cx = string 0
+            let cy = string 0
+            let r = string innerSize
+            tag "circle"
+                [ attr "stroke" stroke
+                  attr "cx" cx
+                  attr "cy" cy
+                  attr "r" r
+                  attr "stroke-width" strokeWidth
+                  attr "fill-opacity" "0.0" ]
+                []
+    let transformVal = transform 0.5 size
+    tag "g"
+        [ attr "transform" transformVal ]
+        [ innerHorizWire
+          innerVertWire
+          innerCircle ]
+
+let swapSymbol (color: Color) (size: float): XmlNode =
+    let stroke = rgb color
+    let strokeWidth = lineWidth size
+    let lineA = let x1 = size |> (*) 0.3 |> string
+                let y1 = size |> (*) 0.3 |> string
+                let x2 = size |> (*) 0.7 |> string
+                let y2 = size |> (*) 0.7 |> string
+                tag "line"
+                    [ attr "stroke" stroke
+                      attr "x1" x1
+                      attr "y1" y1
+                      attr "x2" x2
+                      attr "y2" y2
+                      attr "stroke-width" strokeWidth ]
+                    []
+    let lineB = let x1 = size |> (*) 0.3 |> string
+                let y1 = size |> (*) 0.7 |> string
+                let x2 = size |> (*) 0.7 |> string
+                let y2 = size |> (*) 0.3 |> string
+                tag "line"
+                    [ attr "stroke" stroke
+                      attr "x1" x1
+                      attr "y1" y1
+                      attr "x2" x2
+                      attr "y2" y2
+                      attr "stroke-width" strokeWidth ]
+                    []
+    tag "g" [] [ lineA; lineB ]
+
+let letterBox (fillColor: Color) (strokeColor: Color) (size: float): XmlNode =
+    let rect = let fill =  rgb fillColor
+               let stroke = rgb strokeColor
+               let width = size |> (*) 0.5 |> string
+               let strokeWidth = size |> (*) 0.0025 |> string
+               tag "rect"
+                   [ attr "fill" fill
+                     attr "stroke" stroke
+                     attr "width" width
+                     attr "height" width
+                     attr "stroke-width" strokeWidth ]
+                   []
+    let transformVal = transform 0.25 size
+    tag "g" [ attr "transform" transformVal ] [ rect ]
+
+let letterY (color: Color) (size: float): XmlNode =
+    let stroke = rgb color
+    let strokeWidth = lineWidth size
+    let lineA = let x1 = size |> (*) 0.35 |> string
+                let y1 = size |> (*) 0.35 |> string
+                let x2 = size |> (*) 0.5 |> string
+                let y2 = size |> (*) 0.5 |> string
+                tag "line"
+                    [ attr "stroke" stroke
+                      attr "x1" x1
+                      attr "y1" y1
+                      attr "x2" x2
+                      attr "y2" y2
+                      attr "stroke-width" strokeWidth ]
+                    []
+    let lineB = let x1 = size |> (*) 0.65 |> string
+                let y1 = size |> (*) 0.35 |> string
+                let x2 = size |> (*) 0.5 |> string
+                let y2 = size |> (*) 0.5 |> string
+                tag "line"
+                    [ attr "stroke" stroke
+                      attr "x1" x1
+                      attr "y1" y1
+                      attr "x2" x2
+                      attr "y2" y2
+                      attr "stroke-width" strokeWidth ]
+                    []
+    let lineC = let x1 = size |> (*) 0.5 |> string
+                let y1 = size |> (*) 0.5 |> string
+                let x2 = size |> (*) 0.5 |> string
+                let y2 = size |> (*) 0.65 |> string
+                tag "line"
+                    [ attr "stroke" stroke
+                      attr "x1" x1
+                      attr "y1" y1
+                      attr "x2" x2
+                      attr "y2" y2
+                      attr "stroke-width" strokeWidth ]
+                    []
+    tag "g" [] [ lineA; lineB; lineC ]
+
+let letterZ (color: Color) (size: float): XmlNode =
+    let stroke = rgb color
+    let strokeWidth = lineWidth size
+    let lineA = let x1 = size |> (*) 0.35 |> string
+                let y1 = size |> (*) 0.35 |> string
+                let x2 = size |> (*) 0.65 |> string
+                let y2 = size |> (*) 0.35 |> string
+                tag "line"
+                    [ attr "stroke" stroke
+                      attr "x1" x1
+                      attr "y1" y1
+                      attr "x2" x2
+                      attr "y2" y2
+                      attr "stroke-width" strokeWidth ]
+                    []
+    let lineB = let x1 = size |> (*) 0.65 |> string
+                let y1 = size |> (*) 0.35 |> string
+                let x2 = size |> (*) 0.35 |> string
+                let y2 = size |> (*) 0.65 |> string
+                tag "line"
+                    [ attr "stroke" stroke
+                      attr "x1" x1
+                      attr "y1" y1
+                      attr "x2" x2
+                      attr "y2" y2
+                      attr "stroke-width" strokeWidth ]
+                    []
+    let lineC = let x1 = size |> (*) 0.35 |> string
+                let y1 = size |> (*) 0.65 |> string
+                let x2 = size |> (*) 0.65 |> string
+                let y2 = size |> (*) 0.65 |> string
+                tag "line"
+                    [ attr "stroke" stroke
+                      attr "x1" x1
+                      attr "y1" y1
+                      attr "x2" x2
+                      attr "y2" y2
+                      attr "stroke-width" strokeWidth ]
+                    []
+    tag "g" [] [ lineA; lineB; lineC ]
+
+let ySymbol (fillColor: Color) (color: Color) (size: float): XmlNode =
+    tag "g" [] [ letterBox fillColor color size; letterY color size ]
+
+let zSymbol (fillColor: Color) (color: Color) (size: float): XmlNode =
+    tag "g" [] [ letterBox fillColor color size; letterZ color size ]
---
+++ QuantumPuzzleTest/QuantumPuzzleMechanics.Graphics.ElemsTest.fs
@@ -0,0 +1,125 @@
+﻿module QuantumPuzzleMechanics.Graphics.ElemsTest
+
+open Xunit
+open FsUnit.Xunit
+
+open Giraffe.ViewEngine
+open Xamarin.Forms
+
+open QuantumPuzzleMechanics.Graphics.Elems
+
+[<Fact>]
+let ``Horizontal wire`` () =
+    horizWire Color.Gray 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal """<line stroke="rgb(50, 50, 50)" x1="0" y1="50" x2="100" y2="50" stroke-width="5"></line>"""
+
+[<Fact>]
+let ``Vertical wire`` () =
+    vertWire Color.Red 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal """<line stroke="rgb(100, 0, 0)" x1="50" y1="0" x2="50" y2="100" stroke-width="5"></line>"""
+
+[<Fact>]
+let ``Control symbol`` () =
+    controlSymbol Color.Green 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal """<circle fill="rgb(0, 50, 0)" cx="50" cy="50" r="15"></circle>"""
+
+[<Fact>]
+let ``Not symbol`` () =
+    notSymbol Color.Green 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal
+    <| String.concat
+                ""
+                [| """<g transform="translate(50,50)">"""
+                   """<line stroke="rgb(0, 50, 0)" x1="-20" y1="0" x2="20" y2="0" stroke-width="5"></line>"""
+                   """<line stroke="rgb(0, 50, 0)" x1="0" y1="-20" x2="0" y2="20" stroke-width="5"></line>"""
+                   """<circle stroke="rgb(0, 50, 0)" cx="0" cy="0" r="20" stroke-width="5" fill-opacity="0.0"></circle>"""
+                   """</g>""" |]
+
+[<Fact>]
+let ``Swap symbol`` () =
+    swapSymbol Color.Brown 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal
+    <| String.concat
+                ""
+                [| """<g>"""
+                   """<line stroke="rgb(65, 16, 16)" x1="30" y1="30" x2="70" y2="70" stroke-width="5"></line>"""
+                   """<line stroke="rgb(65, 16, 16)" x1="30" y1="70" x2="70" y2="30" stroke-width="5"></line>"""
+                   """</g>""" |]
+
+[<Fact>]
+let ``Letter box`` () =
+    letterBox Color.Cyan Color.Khaki 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal
+    <| String.concat
+                ""
+                [| """<g transform="translate(25,25)">"""
+                   """<rect fill="rgb(0, 100, 100)" stroke="rgb(94, 90, 55)" width="50" height="50" stroke-width="0.25"></rect>"""
+                   """</g>""" |]
+
+[<Fact>]
+let ``Letter Y`` () =
+    letterY Color.LightGray 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal
+    <| String.concat
+                ""
+                [| """<g>"""
+                   """<line stroke="rgb(83, 83, 83)" x1="35" y1="35" x2="50" y2="50" stroke-width="5"></line>"""
+                   """<line stroke="rgb(83, 83, 83)" x1="65" y1="35" x2="50" y2="50" stroke-width="5"></line>"""
+                   """<line stroke="rgb(83, 83, 83)" x1="50" y1="50" x2="50" y2="65" stroke-width="5"></line>"""
+                   """</g>""" |]
+
+[<Fact>]
+let ``Letter Z`` () =
+    letterZ Color.LightGray 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal
+    <| String.concat
+                ""
+                [| """<g>"""
+                   """<line stroke="rgb(83, 83, 83)" x1="35" y1="35" x2="65" y2="35" stroke-width="5"></line>"""
+                   """<line stroke="rgb(83, 83, 83)" x1="65" y1="35" x2="35" y2="65" stroke-width="5"></line>"""
+                   """<line stroke="rgb(83, 83, 83)" x1="35" y1="65" x2="65" y2="65" stroke-width="5"></line>"""
+                   """</g>""" |]
+
+[<Fact>]
+let ``Y symbol`` () =
+    ySymbol Color.AntiqueWhite Color.Blue 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal
+    <| String.concat
+                ""
+                [| """<g>"""
+                   """<g transform="translate(25,25)">"""
+                   """<rect fill="rgb(98, 92, 84)" stroke="rgb(0, 0, 100)" width="50" height="50" stroke-width="0.25"></rect>"""
+                   """</g>"""
+                   """<g>"""
+                   """<line stroke="rgb(0, 0, 100)" x1="35" y1="35" x2="50" y2="50" stroke-width="5"></line>"""
+                   """<line stroke="rgb(0, 0, 100)" x1="65" y1="35" x2="50" y2="50" stroke-width="5"></line>"""
+                   """<line stroke="rgb(0, 0, 100)" x1="50" y1="50" x2="50" y2="65" stroke-width="5"></line>"""
+                   """</g>"""
+                   """</g>""" |]
+
+[<Fact>]
+let ``Z symbol`` () =
+    zSymbol Color.Blue Color.AntiqueWhite 100.0
+    |> RenderView.AsString.xmlNode
+    |> should equal
+    <| String.concat
+                ""
+                [| """<g>"""
+                   """<g transform="translate(25,25)">"""
+                   """<rect fill="rgb(0, 0, 100)" stroke="rgb(98, 92, 84)" width="50" height="50" stroke-width="0.25"></rect>"""
+                   """</g>"""
+                   """<g>"""
+                   """<line stroke="rgb(98, 92, 84)" x1="35" y1="35" x2="65" y2="35" stroke-width="5"></line>"""
+                   """<line stroke="rgb(98, 92, 84)" x1="65" y1="35" x2="35" y2="65" stroke-width="5"></line>"""
+                   """<line stroke="rgb(98, 92, 84)" x1="35" y1="65" x2="65" y2="65" stroke-width="5"></line>"""
+                   """</g>"""
+                   """</g>""" |]
