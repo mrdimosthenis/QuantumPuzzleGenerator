6cc3473b343133ed16b6f6cec11bd7fdccbfdf38
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-12-27 13:36:33 +0200

    complete selection of set of puzzles

--- QuantumPuzzleGenerator/Selection.fs
+++ QuantumPuzzleGenerator/Selection.fs
@@ -27,3 +27,39 @@ let distinctQStates
     if areNewQstatesDistinct
         then Some allQstates
         else None
+
+let rec selectNextGateAndQStates
+        (random: Random)
+        (numOfQubits: int)
+        (differenceThreshold: Number.Number)
+        (reachedQStates: Matrix.Matrix LazyList)
+    : Quantum.Gate.Gate * Matrix.Matrix LazyList =
+    let candidateGate = Generator.nextGate random numOfQubits ()
+    match distinctQStates numOfQubits differenceThreshold reachedQStates candidateGate with
+    | Some qStates -> (candidateGate, qStates)
+    | None -> selectNextGateAndQStates random numOfQubits differenceThreshold reachedQStates
+
+let selectGatesAndQStates
+        (random: Random)
+        (numOfGates: int)
+        (numOfQubits: int)
+        (differenceThreshold: Number.Number)
+    : Quantum.Gate.Gate LazyList * Matrix.Matrix LazyList =
+
+    let rec go (selectedGates: Quantum.Gate.Gate LazyList)
+               (selectedQStates: Matrix.Matrix LazyList)
+        : Quantum.Gate.Gate LazyList * Matrix.Matrix LazyList =
+        if LazyList.length selectedGates = numOfGates
+            then (selectedGates, selectedQStates)
+            else let (nextGate, nextSelectedQStates) =
+                        selectNextGateAndQStates random
+                                                 numOfQubits
+                                                 differenceThreshold
+                                                 selectedQStates
+                 let nextSelectedGates = LazyList.cons nextGate selectedGates
+                 go nextSelectedGates nextSelectedQStates
+    
+    let initSelectedGates = LazyList.empty
+    let initSelectedQStates =
+            LazyList.ofList [ Generator.nextQState random numOfQubits () ]
+    go initSelectedGates initSelectedQStates
--- QuantumPuzzleTest/QuantumPuzzleGenerator.SelectionTest.fs
+++ QuantumPuzzleTest/QuantumPuzzleGenerator.SelectionTest.fs
@@ -57,3 +57,62 @@ let ``Swap selected as first and second gate does not result distinct quantum st
     distinctQStates numOfQubits differenceThreshold reachedQStates candidateGate
     |> Option.isNone
     |> should equal true
+
+[<Fact>]
+let ``Select one gate and the quantum states of three qubits with  0_01 difference threshold`` () =
+    let (selectedGates, selectedQStates) = selectGatesAndQStates random 1 3 0.01
+    (LazyList.length selectedGates, LazyList.length selectedQStates)
+    |> should equal (1, 2)
+
+[<Fact>]
+let ``Select two gates and the quantum states of four qubits with  0_1 difference threshold`` () =
+    let (selectedGates, selectedQStates) = selectGatesAndQStates random 2 4 0.1
+    (LazyList.length selectedGates, LazyList.length selectedQStates)
+    |> should equal (2, 4)
+
+[<Fact>]
+let ``Select three gates and the quantum states of five qubits with  0_3 difference threshold`` () =
+    let (selectedGates, selectedQStates) = selectGatesAndQStates random 3 5 0.3
+    (LazyList.length selectedGates, LazyList.length selectedQStates)
+    |> should equal (3, 8)
+
+[<Fact>]
+let ``Select three gates of one qubits with  0_01 difference threshold`` () =
+    let randomInstance = System.Random(1000)
+    selectGatesAndQStates randomInstance 3 1 0.01
+    |> fst
+    |> LazyList.toList
+    |> should equal
+        [ Quantum.Gate.YGate 0
+          Quantum.Gate.ZGate 0
+          Quantum.Gate.XGate 0]
+
+[<Fact>]
+let ``Select four gates of five qubits with  0_2 difference threshold`` () =
+    let randomInstance = System.Random(1000)
+    selectGatesAndQStates randomInstance 4 5 0.2
+    |> fst
+    |> LazyList.toList
+    |> should equal
+        [ Quantum.Gate.YGate 1
+          Quantum.Gate.ZGate 4
+          Quantum.Gate.CXGate (1, 2)
+          Quantum.Gate.CSwapGate (4, 3, 1) ]
+
+[<Fact>]
+let ``Select 10 gates of four qubits with  0_2 difference threshold`` () =
+    let randomInstance = System.Random(1000)
+    selectGatesAndQStates randomInstance 10 4 0.2
+    |> fst
+    |> LazyList.toList
+    |> should equal
+        [ Quantum.Gate.CXGate (1, 0)
+          Quantum.Gate.CSwapGate (2, 1, 0)
+          Quantum.Gate.CZGate (1, 0)
+          Quantum.Gate.XGate 0
+          Quantum.Gate.CCXGate (0, 2, 3)
+          Quantum.Gate.CXGate (1, 3)
+          Quantum.Gate.SwapGate (1, 0)
+          Quantum.Gate.XGate 3
+          Quantum.Gate.CSwapGate (0, 1, 3)
+          Quantum.Gate.SwapGate (3, 0) ]
