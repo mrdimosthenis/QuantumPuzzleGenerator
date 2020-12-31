1fce18c2909d1373b63ec0d375b833d9326e5a6d
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-12-31 14:43:44 +0200

    add gate png files

---
+++ QuantumPuzzleGenerator/Resources.fs
@@ -0,0 +1,10 @@
+﻿module QuantumPuzzleGenerator.Resources
+
+open Fabulous.XamarinForms
+open Xamarin.Forms
+
+let image (name: string): Image.Value = 
+    name
+    |> sprintf "QuantumPuzzleGenerator.resources.images.%s.png"
+    |> ImageSource.FromResource
+    |> Image.fromImageSource
