243da53e04752331069c9e72de55429179f56320
mrdimosthenis <mrdimosthenis@hotmail.com>
2021-01-04 13:52:17 +0200

    create vertical csv files

--- QuantumPuzzleTest/CustomScripts.CreateSvgFiles.fs
+++ QuantumPuzzleTest/CustomScripts.CreateSvgFiles.fs
@@ -1,6 +1,5 @@
 ﻿module CustomScripts.CreateSvgFiles
 
-open System
 open System.IO
 
 open Xunit
@@ -21,20 +20,18 @@ let size: float = 256.0
 let strokeColor: Color = Color.Gray
 
 let svgContent (numOfQubits: int) (gate: Quantum.Gate.Gate): string =
-    let width = numOfQubits
-                |> float
-                |> (*) size
-                |> int
-                |> string
-    let height = size
-                 |> int
-                 |> string
+    let width = size |> int |> string
+
+    let height =
+        numOfQubits |> float |> (*) size |> int |> string
+
     svg [ Version "1.1"
           SVGAttr.Custom("baseProfile", "full")
           SVGAttr.Custom("width", width)
           SVGAttr.Custom("height", height)
-          SVGAttr.Custom("xmlns", "http://www.w3.org/2000/svg") ]
-        [ Graphics.Gates.gateGraphics numOfQubits gate strokeColor size ]
+          SVGAttr.Custom("xmlns", "http://www.w3.org/2000/svg") ] [
+        Graphics.Gates.gateGraphics numOfQubits gate strokeColor size
+    ]
     |> ReactServer.renderToString
 
 let gateName (numOfQubits: int) (gate: Quantum.Gate.Gate): string =
@@ -50,41 +47,46 @@ let gateName (numOfQubits: int) (gate: Quantum.Gate.Gate): string =
     | Quantum.Gate.CSwapGate (ia, ib, ic) -> sprintf "csw%i%i%i%i" numOfQubits ia ib ic
 
 let saveGate (numOfQubits: int) (gate: Quantum.Gate.Gate): unit =
-    let filePath = gateName numOfQubits gate
-                   |> sprintf "%s\%s.svg" dirPath
-    let content = svgContent numOfQubits gate
-    File.WriteAllLines (filePath, [content])
+    let filePath =
+        gateName numOfQubits gate
+        |> sprintf "%s\%s.svg" dirPath
 
-let saveAllSingeQubitGates(): unit =
+    let content = svgContent numOfQubits gate
+    File.WriteAllLines(filePath, [ content ])
+
+let saveAllSingeQubitGates (): unit =
     for numOfQubits = 1 to maxNumOfQubits do
-        for g in [ Quantum.Gate.XGate; Quantum.Gate.YGate; Quantum.Gate.ZGate; Quantum.Gate.HGate ] do
-           for i = 0 to (numOfQubits-1) do
+        for g in [ Quantum.Gate.XGate
+                   Quantum.Gate.YGate
+                   Quantum.Gate.ZGate
+                   Quantum.Gate.HGate ] do
+            for i = 0 to (numOfQubits - 1) do
                 saveGate numOfQubits (g i)
 
-let saveAllDoubleQubitGates(): unit =
+let saveAllDoubleQubitGates (): unit =
     for numOfQubits = 2 to maxNumOfQubits do
-        for g in [ Quantum.Gate.SwapGate; Quantum.Gate.CXGate; Quantum.Gate.CZGate ] do
-           for ia = 0 to (numOfQubits-1) do
-                for ib = 0 to (numOfQubits-1) do
-                    if ib <> ia
-                        then saveGate numOfQubits (g (ia, ib))
-                        else ()
+        for g in [ Quantum.Gate.SwapGate
+                   Quantum.Gate.CXGate
+                   Quantum.Gate.CZGate ] do
+            for ia = 0 to (numOfQubits - 1) do
+                for ib = 0 to (numOfQubits - 1) do
+                    if ib <> ia then saveGate numOfQubits (g (ia, ib)) else ()
 
-let saveAllTripleQubitGates(): unit =
+let saveAllTripleQubitGates (): unit =
     for numOfQubits = 3 to maxNumOfQubits do
-        for g in [ Quantum.Gate.CCXGate; Quantum.Gate.CSwapGate ] do
-           for ia = 0 to (numOfQubits-1) do
-                for ib = 0 to (numOfQubits-1) do
-                    if ib <> ia
-                        then for ic = 0 to (numOfQubits-1) do
-                                if ic <> ia && ic <> ib
-                                    then saveGate numOfQubits (g (ia, ib, ic))
-                                    else ()
-                        else ()
+        for g in [ Quantum.Gate.CCXGate
+                   Quantum.Gate.CSwapGate ] do
+            for ia = 0 to (numOfQubits - 1) do
+                for ib = 0 to (numOfQubits - 1) do
+                    if ib <> ia then
+                        for ic = 0 to (numOfQubits - 1) do
+                            if ic <> ia && ic <> ib then saveGate numOfQubits (g (ia, ib, ic)) else ()
+                    else
+                        ()
 
 //[<Fact>]
 //let ``save all qubit gates`` () =
-//    saveAllSingeQubitGates()
-//    saveAllDoubleQubitGates()
-//    saveAllTripleQubitGates()
+//    saveAllSingeQubitGates ()
+//    saveAllDoubleQubitGates ()
+//    saveAllTripleQubitGates ()
 //    should equal () ()
