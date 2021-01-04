0600600823538768d1eb9159fdf279a9b050ecca
mrdimosthenis <mrdimosthenis@hotmail.com>
2021-01-04 15:42:13 +0200

    gate selection is reversed

--- QuantumPuzzleGenerator/Model.fs
+++ QuantumPuzzleGenerator/Model.fs
@@ -7,29 +7,30 @@ open QuantumPuzzleMechanics
 
 // types
 
-type Msg = | GateClick of int
+type Msg = GateClick of int
 
-type Model = { Level: int
-               InitQState: Matrix.Matrix
-               TargetIndex: int
-               TargetQStates: Matrix.Matrix list
-               GateSelection: bool list
-               Gates: Quantum.Gate.Gate list }
+type Model =
+    { Level: int
+      InitQState: Matrix.Matrix
+      TargetIndex: int
+      TargetQStates: Matrix.Matrix list
+      GateSelection: bool list
+      Gates: Quantum.Gate.Gate list }
 
 // update function
 
 let update (msg: Msg) (model: Model) =
     match msg with
     | GateClick index ->
-        let newGateSelection = model.GateSelection
-                               |> List.indexed
-                               |> List.map
-                                    (fun (i, b) ->
-                                        if i = index
-                                            then not b
-                                            else b
-                                    )
-        let newModel = { model with GateSelection = newGateSelection }
+        let newGateSelection =
+            model.GateSelection
+            |> List.indexed
+            |> List.map (fun (i, b) -> if i = index then not b else b)
+
+        let newModel =
+            { model with
+                  GateSelection = newGateSelection }
+
         newModel, Cmd.none
 
 // model functions
@@ -61,28 +62,23 @@ let numOfGates (model: Model): int =
 
 let initModel (): Model =
     let random = System.Random()
-    let (gateLazyList, qStates) =
-            Selection.selectGatesAndQStates
-                        random
-                        1
-                        3
-                        1
-                        0.2
-    let (initQState, otherQStates) = qStates
-                                     |> LazyList.rev
-                                     |> LazyList.uncons
+
+    let (gatesRev, qStates) =
+        Selection.selectGatesAndQStates random 1 3 1 0.2
+
+    let (initQState, otherQStates) =
+        qStates |> LazyList.rev |> LazyList.uncons
+
     let targetQStates =
-            Generator.nextDistinctGroup
-                random
-                (LazyList.length otherQStates)
-                4
-                ()
-            |> List.map (fun i -> Seq.item i otherQStates)
-    let gates = LazyList.toList gateLazyList
+        Generator.nextDistinctGroup random (LazyList.length otherQStates) 4 ()
+        |> List.map (fun i -> Seq.item i otherQStates)
+
+    let gates =
+        gatesRev |> LazyList.rev |> LazyList.toList
+
     { Level = 1
       InitQState = initQState
       TargetIndex = 0
       TargetQStates = targetQStates
       GateSelection = List.replicate gates.Length false
       Gates = gates }
- 
