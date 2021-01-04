02fde4a62fc94b9930c22d11a50e7b6d5bea91c0
mrdimosthenis <mrdimosthenis@hotmail.com>
2021-01-04 14:32:31 +0200

    horizontal circuit scrolling

--- QuantumPuzzleGenerator/Pages.Play.fs
+++ QuantumPuzzleGenerator/Pages.Play.fs
@@ -7,39 +7,48 @@ open Xamarin.Forms
 open QuantumPuzzleMechanics
 open QuantumPuzzleGenerator
 
-let gateButtons (model: Model.Model) (dispatch: Model.Msg -> unit) : ViewElement list =
-    List.zip model.Gates model.GateSelection
-    |> List.indexed
-    |> List.map
-        (fun (i, (g, b)) ->
-            View.ImageButton(
-                source = (model |> Model.numOfQubits |> Resources.gateImage g),
-                backgroundColor = (if b then Color.LightGreen else Color.Transparent),
-                command = fun () -> i |> Model.GateClick |> dispatch
-            )
-        )
+let gateButtons (model: Model.Model) (dispatch: Model.Msg -> unit): ViewElement =
+    let children =
+        List.zip model.Gates model.GateSelection
+        |> List.indexed
+        |> List.map (fun (i, (g, b)) ->
+            View.ImageButton
+                (source =
+                    (model
+                     |> Model.numOfQubits
+                     |> Resources.gateImage g),
+                 backgroundColor = (if b then Color.LightGreen else Color.Transparent),
+                 command = fun () -> i |> Model.GateClick |> dispatch))
 
-let stackLayout (model: Model.Model) (dispatch: Model.Msg -> unit) : ViewElement =
+    View.ScrollView
+        (orientation = ScrollOrientation.Horizontal,
+         content =
+             View.StackLayout
+                 (orientation = StackOrientation.Horizontal,
+                  horizontalOptions = LayoutOptions.Center,
+                  verticalOptions = LayoutOptions.Center,
+                  children = children))
+
+let stackLayout (model: Model.Model) (dispatch: Model.Msg -> unit): ViewElement =
     let numOfQubits = Model.numOfQubits model
+
     let goalQStatePlot =
-            model.TargetQStates.[model.TargetIndex]
-            |> QStatePlotting.webView numOfQubits
+        model.TargetQStates.[model.TargetIndex]
+        |> QStatePlotting.webView numOfQubits
+
     let currentQStatePlot =
-            List.zip model.Gates model.GateSelection
-            |> List.filter snd
-            |> List.map fst
-            |> List.fold
-                 (fun qState gate ->
-                         let gateMatrix = Quantum.Gate.matrix numOfQubits gate
-                         Matrix.standardProduct gateMatrix qState
-                 )
-                 model.InitQState
-            |> QStatePlotting.webView numOfQubits
-    let children = List.concat [ [ goalQStatePlot ]
-                                 gateButtons model dispatch
-                                 [ currentQStatePlot ] ]
-    View.StackLayout(
-        horizontalOptions = LayoutOptions.Center,
-        verticalOptions = LayoutOptions.Center,
-        children = children
-    )
+        List.zip model.Gates model.GateSelection
+        |> List.filter snd
+        |> List.map fst
+        |> List.fold (fun qState gate ->
+            let gateMatrix = Quantum.Gate.matrix numOfQubits gate
+            Matrix.standardProduct gateMatrix qState) model.InitQState
+        |> QStatePlotting.webView numOfQubits
+
+    View.StackLayout
+        (horizontalOptions = LayoutOptions.Center,
+         verticalOptions = LayoutOptions.Center,
+         children =
+             [ goalQStatePlot
+               gateButtons model dispatch
+               currentQStatePlot ])
--- QuantumPuzzleGenerator/QStatePlotting.fs
+++ QuantumPuzzleGenerator/QStatePlotting.fs
@@ -17,24 +17,19 @@ open Newtonsoft.Json.Serialization
 
 type SpaceCoordinates = float * float * float
 
-type Style = {
-    Fill: string
-    Stroke: string
-}
+type Style = { Fill: string; Stroke: string }
 
-type Row = {
-    X: float
-    Y: float
-    Z: float
-    Style: Style
-}
+type Row =
+    { X: float
+      Y: float
+      Z: float
+      Style: Style }
 
 type Data = Row list
 
-type GeneratedByQuantumPuzzles = {
-    DotSizeRatioScale: float
-    Data: Data
-}
+type GeneratedByQuantumPuzzles =
+    { DotSizeRatioScale: float
+      Data: Data }
 
 // exceptions
 
@@ -43,8 +38,7 @@ exception InvalidNumOfQubits
 // constants
 
 let coordsForOneQubit: SpaceCoordinates LazyList =
-    [ (0.25, 0.5, 0.5)
-      (0.75, 0.5, 0.5) ]
+    [ (0.25, 0.5, 0.5); (0.75, 0.5, 0.5) ]
     |> LazyList.ofList
 
 let coordsForTwoQubits: SpaceCoordinates LazyList =
@@ -95,97 +89,98 @@ let coordsForQubits (numOfQubits: int): SpaceCoordinates LazyList =
     | _ -> raise InvalidNumOfQubits
 
 let fillColor (c: Complex.Complex): Forms.Color =
-    let v = c
-            |> Complex.toPolar
-            |> fst
-            |> (*) -256.0
-            |> (+) 256.0
-            |> Math.Floor
-            |> int
+    let v =
+        c
+        |> Complex.toPolar
+        |> fst
+        |> (*) -256.0
+        |> (+) 256.0
+        |> Math.Floor
+        |> int
+
     Forms.Color.FromRgb(v, v, v)
 
 let strokeColor (c: Complex.Complex): Forms.Color =
-    if c = Complex.zero
-        then Forms.Color.White
-        else let h = c
-                     |> Complex.toPolar
-                     |> snd
-                     |> (+) Math.PI
-                     |> (*) (0.5 / Math.PI)
-             Forms.Color.FromHsv(h, 1.0, 1.0)
+    if c = Complex.zero then
+        Forms.Color.White
+    else
+        let h =
+            c
+            |> Complex.toPolar
+            |> snd
+            |> (+) Math.PI
+            |> (*) (0.5 / Math.PI)
+
+        Forms.Color.FromHsv(h, 1.0, 1.0)
 
 let data (numOfQubits: int) (qState: Matrix.Matrix): Data =
     qState
     |> LazyList.concat
     |> LazyList.zip (coordsForQubits numOfQubits)
-    |> LazyList.map
-            (fun ((x, y, z), complex) -> {
-                    X = x
-                    Y = y
-                    Z = z
-                    Style = {
-                        Fill = complex |> fillColor |> Graphics.Elems.rgb
-                        Stroke = complex |> strokeColor |> Graphics.Elems.rgb
-                    }
-                }
-            )
+    |> LazyList.map (fun ((x, y, z), complex) ->
+        { X = x
+          Y = y
+          Z = z
+          Style =
+              { Fill = complex |> fillColor |> Graphics.Elems.rgb
+                Stroke = complex |> strokeColor |> Graphics.Elems.rgb } })
     |> LazyList.toList
 
-let generatedByQuantumPuzzles
-        (numOfQubits: int)
-        (qState: Matrix.Matrix)
-    : string =
+let generatedByQuantumPuzzles (numOfQubits: int) (qState: Matrix.Matrix): string =
     let dotSizeRatioScale =
-            match numOfQubits with
-            | 1 -> 4.0
-            | 2 -> 3.0
-            | 3 -> 2.0
-            | 4 -> 1.0
-            | _ -> raise InvalidNumOfQubits
-    let content = {
-        DotSizeRatioScale = dotSizeRatioScale
-        Data = data numOfQubits qState
-    }
+        match numOfQubits with
+        | 1 -> 4.0
+        | 2 -> 3.0
+        | 3 -> 2.0
+        | 4 -> 1.0
+        | _ -> raise InvalidNumOfQubits
+
+    let content =
+        { DotSizeRatioScale = dotSizeRatioScale
+          Data = data numOfQubits qState }
+
     let serializerSettings = new JsonSerializerSettings()
     serializerSettings.ContractResolver <- new CamelCasePropertyNamesContractResolver()
     JsonConvert.SerializeObject(content, serializerSettings)
 
-let singlePlotHtmlString
-        (numOfQubits: int)
-        (qState: Matrix.Matrix)
-    : string =
+let singlePlotHtmlString (numOfQubits: int) (qState: Matrix.Matrix): string =
     let visJs = Resources.text "vis_graph3d_601.js"
-    let dataJs = generatedByQuantumPuzzles numOfQubits qState
-                 |> sprintf "var generatedByQuantumPuzzles = %s;"
+
+    let dataJs =
+        generatedByQuantumPuzzles numOfQubits qState
+        |> sprintf "var generatedByQuantumPuzzles = %s;"
+
     let drawJs = Resources.text "single_plot.js"
-    html []
-         [ head [] [ meta
-                        [ Name "viewport"
-                          HTMLAttr.Custom("content", "width=device-width, initial-scale=1") ]
-                     Standard.style
-                        []
-                        [ RawText "* { margin: 0; }" ]
-                     script
-                        [ Type "text/javascript" ]
-                        [ RawText visJs ]
-                     script
-                        [ Type "text/javascript" ]
-                        [ RawText dataJs ]
-                     script
-                        [ Type "text/javascript" ]
-                        [ RawText drawJs ] ]
-           body
-            [ HTMLAttr.Custom("onload", "drawVisualization();") ]
-            [ div [ Id "mygraph"] [] ] ]
+
+    html [] [
+        head [] [
+            meta [ Name "viewport"
+                   HTMLAttr.Custom("content", "width=device-width, initial-scale=1") ]
+            Standard.style [] [
+                RawText "* { margin: 0; }"
+            ]
+            script [ Type "text/javascript" ] [
+                RawText visJs
+            ]
+            script [ Type "text/javascript" ] [
+                RawText dataJs
+            ]
+            script [ Type "text/javascript" ] [
+                RawText drawJs
+            ]
+        ]
+        body [ HTMLAttr.Custom("onload", "drawVisualization();") ] [
+            div [ Id "mygraph" ] []
+        ]
+    ]
     |> ReactServer.renderToString
 
-let webView
-        (numOfQubits: int)
-        (qState: Matrix.Matrix)
-    : ViewElement =
+let webView (numOfQubits: int) (qState: Matrix.Matrix): ViewElement =
     let htmlString = singlePlotHtmlString numOfQubits qState
-    View.WebView(
-        source=Xamarin.Forms.HtmlWebViewSource(Html=htmlString),
-        width=600.0,
-        height=600.0
-    )
+
+    View.WebView
+        (source = Xamarin.Forms.HtmlWebViewSource(Html = htmlString),
+         width = 300.0,
+         height = 300.0,
+         horizontalOptions = Forms.LayoutOptions.Center,
+         verticalOptions = Forms.LayoutOptions.Center)
