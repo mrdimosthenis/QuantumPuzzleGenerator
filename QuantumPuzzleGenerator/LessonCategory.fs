0d259c22cf9ba93aa5cd8743f6769b4296317808
mrdimosthenis <mrdimosthenis@hotmail.com>
2021-01-07 20:18:45 +0200

    invert the gate indices in lessons

--- QuantumPuzzleGenerator/LessonCategory.fs
+++ QuantumPuzzleGenerator/LessonCategory.fs
@@ -65,86 +65,86 @@ let lessonCategories: LessonCategory list =
       ("⊕ for One Qubit", 1, [ Quantum.Gate.XGate 0 ], false, false)
       ("⊕ for One of Two Qubits",
        2,
-       [ Quantum.Gate.XGate 0
-         Quantum.Gate.XGate 1 ],
+       [ Quantum.Gate.XGate 1
+         Quantum.Gate.XGate 0 ],
        false,
        false)
       ("⊕ for One of Three Qubits",
        3,
-       [ Quantum.Gate.XGate 0
+       [ Quantum.Gate.XGate 2
          Quantum.Gate.XGate 1
-         Quantum.Gate.XGate 2 ],
+         Quantum.Gate.XGate 0 ],
        false,
        false)
 
       ("Z for One Qubit", 1, [ Quantum.Gate.ZGate 0 ], false, true)
       ("Z for One of Two Qubits",
        2,
-       [ Quantum.Gate.ZGate 0
-         Quantum.Gate.ZGate 1 ],
+       [ Quantum.Gate.ZGate 1
+         Quantum.Gate.ZGate 0 ],
        false,
        true)
       ("Z for One of Three Qubits",
        3,
-       [ Quantum.Gate.ZGate 0
+       [ Quantum.Gate.ZGate 2
          Quantum.Gate.ZGate 1
-         Quantum.Gate.ZGate 2 ],
+         Quantum.Gate.ZGate 0 ],
        false,
        true)
 
       ("Y for One Qubit", 1, [ Quantum.Gate.YGate 0 ], false, true)
       ("Y for One of Two Qubits",
        2,
-       [ Quantum.Gate.YGate 0
-         Quantum.Gate.YGate 1 ],
+       [ Quantum.Gate.YGate 1
+         Quantum.Gate.YGate 0 ],
        false,
        true)
       ("Y for One of Three Qubits",
        3,
-       [ Quantum.Gate.YGate 0
+       [ Quantum.Gate.YGate 2
          Quantum.Gate.YGate 1
-         Quantum.Gate.YGate 2 ],
+         Quantum.Gate.YGate 0 ],
        false,
        true)
 
       ("H for One Qubit", 1, [ Quantum.Gate.HGate 0 ], false, true)
       ("H for One of Two Qubits",
        2,
-       [ Quantum.Gate.HGate 0
-         Quantum.Gate.HGate 1 ],
+       [ Quantum.Gate.HGate 1
+         Quantum.Gate.HGate 0 ],
        false,
        true)
       ("H for One of Three Qubits",
        3,
-       [ Quantum.Gate.HGate 0
+       [ Quantum.Gate.HGate 2
          Quantum.Gate.HGate 1
-         Quantum.Gate.HGate 2 ],
+         Quantum.Gate.HGate 0 ],
        false,
        true)
 
-      ("Swap for Two Qubits", 2, [ Quantum.Gate.SwapGate(0, 1) ], false, false)
+      ("Swap for Two Qubits", 2, [ Quantum.Gate.SwapGate(1, 0) ], false, false)
       ("Swap for Two of Three Qubits",
        3,
-       [ Quantum.Gate.SwapGate(0, 1)
+       [ Quantum.Gate.SwapGate(1, 2)
          Quantum.Gate.SwapGate(0, 2)
-         Quantum.Gate.SwapGate(1, 2) ],
+         Quantum.Gate.SwapGate(0, 1) ],
        false,
        false)
 
       ("Controlled ⊕ for Two Qubits",
        2,
-       [ Quantum.Gate.CXGate(0, 1)
-         Quantum.Gate.CXGate(1, 0) ],
+       [ Quantum.Gate.CXGate(1, 0)
+         Quantum.Gate.CXGate(0, 1) ],
        false,
        false)
       ("Controlled ⊕ for Two of Three Qubits",
        3,
-       [ Quantum.Gate.CXGate(0, 1)
-         Quantum.Gate.CXGate(1, 0)
-         Quantum.Gate.CXGate(0, 2)
-         Quantum.Gate.CXGate(2, 0)
+       [ Quantum.Gate.CXGate(2, 1)
          Quantum.Gate.CXGate(1, 2)
-         Quantum.Gate.CXGate(2, 1) ],
+         Quantum.Gate.CXGate(2, 0)
+         Quantum.Gate.CXGate(0, 2)
+         Quantum.Gate.CXGate(1, 0)
+         Quantum.Gate.CXGate(0, 1) ],
        false,
        false) ]
     |> List.indexed
