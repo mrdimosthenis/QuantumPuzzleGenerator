152f1418a942c295b3878533e96c3356965a6dc4
MariaBat <MariaBat>
2021-01-26 19:56:02 +0200

    introduce openUrl function in credits page

--- QuantumPuzzleGenerator/Pages.Credits.fs
+++ QuantumPuzzleGenerator/Pages.Credits.fs
@@ -6,17 +6,18 @@ open Xamarin.Essentials
 
 open QuantumPuzzleGenerator
 
-let openGitHub () =
-    Uri("https://github.com/mrdimosthenis/QuantumPuzzleGenerator")
+let openUrl (url: string): unit =
+    url
+    |> Uri
     |> Launcher.OpenAsync
     |> Async.AwaitTask
     |> Async.StartImmediate
 
-let openLinkedIn () =
-    Uri("https://www.linkedin.com/in/mrdimosthenis/")
-    |> Launcher.OpenAsync
-    |> Async.AwaitTask
-    |> Async.StartImmediate
+let openGitHub (): unit =
+    openUrl "https://github.com/mrdimosthenis/QuantumPuzzleGenerator"
+
+let openLinkedIn (): unit =
+    openUrl "https://www.linkedin.com/in/mrdimosthenis/"
 
 let descriptionLbl (): ViewElement =
     "Quantum Puzzle Generator was created by Dimosthenis Michailidis"
@@ -25,12 +26,12 @@ let descriptionLbl (): ViewElement =
 let codeBtn (): ViewElement =
     let imageNameOpt = Some "icons.code"
 
-    UIComponents.button "Source Code" imageNameOpt openGitHub
+    UIComponents.button "Code on GitHub" imageNameOpt openGitHub
 
 let devBtn (): ViewElement =
     let imageNameOpt = Some "icons.profile"
 
-    UIComponents.button "Developer" imageNameOpt openLinkedIn
+    UIComponents.button "Developer on LinkedIn" imageNameOpt openLinkedIn
 
 let versionLbl (): ViewElement =
     Constants.version
