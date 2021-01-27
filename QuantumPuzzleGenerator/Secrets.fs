d3ee252dcf6eb6c4e4afa474ba509480fafb702b
MariaBat <MariaBat>
2021-01-27 13:42:22 +0200

    add internal secrets module

---
+++ QuantumPuzzleGenerator/Secrets.fs
@@ -0,0 +1,7 @@
+﻿module internal QuantumPuzzleGenerator.Secrets
+
+let internal secrets: Map<string, string> =
+    [ ("password", "12345")
+      ("token", "12345")
+      ("secret", "12345") ]
+    |> Map.ofList
