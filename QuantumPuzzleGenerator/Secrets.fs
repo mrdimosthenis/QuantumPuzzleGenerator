4f074742f7443525f2acd6de73f9fab28058e9d3
MariaBat <MariaBat>
2021-01-30 22:40:13 +0200

    git add secrets

---
+++ QuantumPuzzleGenerator/Secrets.fs
@@ -0,0 +1,6 @@
+﻿module internal QuantumPuzzleGenerator.Secrets
+
+let internal secrets: Map<string, string> =
+    [ ("AppCenterIOSSecret", "12345")
+      ("AppCenterAndroidSecret", "12345") ]
+    |> Map.ofList
