aa3add42508d9cce82831e67b170795283145092
MariaBat <MariaBat>
2021-01-30 22:42:57 +0200

    .gitignore is now working

--- QuantumPuzzleGenerator/Secrets.fs
+++
@@ -1,6 +0,0 @@
-﻿module internal QuantumPuzzleGenerator.Secrets
-
-let internal secrets: Map<string, string> =
-    [ ("AppCenterIOSSecret", "12345")
-      ("AppCenterAndroidSecret", "12345") ]
-    |> Map.ofList
